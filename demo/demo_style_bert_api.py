fc13ad8e6782c332af8206b7b412efcbb9bde608
Maki <haru.maki.ch.0303@gmail.com>
2024-03-30 15:11:47 +0900

    Update demo_style_bert_api.py

--- demo/demo_style_bert_api.py
+++ demo/demo_style_bert_api.py
@@ -14,4 +14,4 @@ tts = TextToSpeech(api_url="http://style-bert-vits2-api:5000/voice")
 # テキストを指定して音声を生成
 text = "こんにちは、今日の気分はどうですか？"
 text = "なるほど、Transformerを画像に使うんですね！位置情報って具体的にはどういうことですか？"
-tts.generate_audio(text, model_id=5)
+tts.generate_audio(text, model_id=4)
